8f1099ffe8c6486cab161d9a25db4eff6e940552
Felipe Nantes <felipenantes1303@gmail.com>
2026-05-07 16:57:43 -0400

    Lógica da tela de registro finalizada

---
+++ js/login.js
@@ -0,0 +1,7 @@
+const email = document.getElementById('email');
+const senha = document.getElementById('senha');
+const user = users.find(user => user.email)
+
+if(email === user.email && senha === user.senha){
+
+}
