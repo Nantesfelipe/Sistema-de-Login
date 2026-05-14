c6c7325e5b42c67020ea6ab3aaf4522c1159f356
Felipe Nantes <felipenantes1303@gmail.com>
2026-05-14 16:03:23 -0400

    Finalização do sistema de Login front-end!

--- js/login.js
+++ js/login.js
@@ -1,7 +1,33 @@
-const email = document.getElementById('email');
-const senha = document.getElementById('senha');
-const user = users.find(user => user.email)
+const form = document.querySelector('#formLogin');
+const mensagem = document.querySelector('.container');
+const users = JSON.parse(localStorage.getItem("users") || []);
+ 
 
-if(email === user.email && senha === user.senha){
 
-}
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const emailLogin = document.querySelector('#email').value;
+    const senhaLogin = document.querySelector('#senha').value;
+
+    const user = users.find(user => user.email === emailLogin); //dicar para ler Arrow Function de forma lógica: Para cada user => o email dele é igual ao digitado ?
+
+    if (user && user.senha === senhaLogin) {
+
+
+        mensagem.innerHTML = "<h2 style='color:green; text-align: center;'>Login bem sucedido!</h2>";
+        setTimeout(() => {
+            window.location.href = "dashboard.html";
+        }, 2000); 
+
+      
+
+    } else {
+        mensagem.innerHTML = "<h2 style='color:red; text-align: center;'>Usuário inválido</h2>";
+    }
+
+
+});
+
+
+
